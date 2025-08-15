9a2376faa1ef8e9d81ff39ae20f2e4d0aa7a1e4e
Heath Babb <hbabb@heath-babb.dev>
2025-08-15 18:24:27 -0400

    🎉 init: initialize pnpm workspace and add configuration files
    - Added pnpm-workspace.yaml to define package structure for apps and packages.
    - Created renovate.json for automated dependency management and scheduling.
    - Introduced tsconfig.json for TypeScript path mapping and compiler options.
    - Added vercel.json to configure Git deployment settings for Vercel.

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import antfu from "@antfu/eslint-config";
+
+// TODO: add tailwindcss plugin
+
+export default antfu({
+  // Your custom configs here
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+    objectCurlySpacing: "always",
+    arrowParens: "always",
+    linebreaks: "unix",
+  },
+  ignores: [".pnpm-store/**", "node_modules/**", "**/migrations/*", "docs/**"],
+}, {
+  rules: {
+    "vue/max-attributes-per-line": ["error", {
+      singleline: {
+        max: 2,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+    "ts/no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      cases: {
+        camelCase: true,
+        kebabCase: true,
+      },
+      ignore: ["README.md"],
+    }],
+  },
+});
